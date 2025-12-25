5376a92973e7126c739a39ff52a97762f3939c13
Gideon Cameron <gideonwork20@gmail.com>
2025-12-25 04:08:21 -0800

    Changed the favicon

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./Components/Hero";
 import About from "./Components/About";
 import Services from "./Components/Services";
 import Work from "./Components/Work";
+import Contact from "./Components/Contact";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <About />
       <Services />
       <Work />
+      <Contact />
 
       {/*
         Future sections will go here:
